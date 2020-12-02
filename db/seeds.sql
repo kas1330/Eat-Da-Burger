02e518d85dfeba02e90ae38a655c2a1725610de1
Kerri Sexton <k.sexton804@gmail.com>
2020-12-01 21:26:09 -0500

    db files done

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hawaiian Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chili Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', false);
+
