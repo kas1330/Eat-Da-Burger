454282c1dca4444ffd7d7b39cb7bfb176eaac81e
Kerri Sexton <k.sexton804@gmail.com>
2020-12-03 00:49:38 -0500

    added more to server.js

--- server.js
+++ server.js
@@ -1,3 +1,24 @@
 const mysql = require('mysql');
 const express = require('express');
-const expressHandle = require('express-handlebars');
+const exphbs = require('express-handlebars');
+
+var app = express();
+
+
+var PORT = process.env.PORT || 8080;
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require('./controllers/burgers_controller.js');
+app.use(routes);
+
+app.listen(PORT, function () {
+    console.log('App listening on PORT: ' + PORT);
+  });
